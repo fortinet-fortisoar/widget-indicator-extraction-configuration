d2152191ecee3a76b0896c3e191dec3ca6da1fef
cs-rohit-khune <rkhune@fortinet.com>
2024-05-23 10:22:38 +0530

    Added First Page

--- widget/view.controller.js
+++ widget/view.controller.js
@@ -3,15 +3,18 @@
   All rights reserved.
   FORTINET CONFIDENTIAL & FORTINET PROPRIETARY SOURCE CODE
   Copyright end */
+  
 'use strict';
 (function () {
     angular
       .module('cybersponse')
       .controller('soarFrameworkConfigurationWizard100Ctrl', soarFrameworkConfigurationWizard100Ctrl);
 
-    soarFrameworkConfigurationWizard100Ctrl.$inject = ['$scope', 'widgetUtilityService'];
+    soarFrameworkConfigurationWizard100Ctrl.$inject = ['$scope', 'widgetUtilityService', '$rootScope', 'widgetBasePath','$controller'];
 
-    function soarFrameworkConfigurationWizard100Ctrl($scope, widgetUtilityService) {
+    function soarFrameworkConfigurationWizard100Ctrl($scope, widgetUtilityService, $rootScope, widgetBasePath, $controller) {
+      $scope.isLightTheme = $rootScope.theme.id === 'light';
+      $scope.startPageImage = $scope.isLightTheme ? widgetBasePath +'images/sfsp-start-light.png': widgetBasePath +'images/sfsp-start-dark.png';
 
       function _handleTranslations() {
         widgetUtilityService.checkTranslationMode($scope.$parent.model.type).then(function () {
