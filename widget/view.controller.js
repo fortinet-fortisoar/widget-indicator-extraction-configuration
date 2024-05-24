b2565ac5e8bebc6a20dbf0134c60931955e62eee
cs-rohit-khune <rkhune@fortinet.com>
2024-05-24 09:39:23 +0530

    Added Welcome Page

--- widget/view.controller.js
+++ widget/view.controller.js
@@ -9,11 +9,12 @@
       .module('cybersponse')
       .controller('soarFrameworkConfigurationWizard100Ctrl', soarFrameworkConfigurationWizard100Ctrl);
 
-    soarFrameworkConfigurationWizard100Ctrl.$inject = ['$scope', 'widgetUtilityService', '$rootScope', 'widgetBasePath','$controller'];
+    soarFrameworkConfigurationWizard100Ctrl.$inject = ['$scope', 'widgetUtilityService', '$rootScope', 'widgetBasePath', 'WizardHandler'];
 
-    function soarFrameworkConfigurationWizard100Ctrl($scope, widgetUtilityService, $rootScope, widgetBasePath, $controller) {
+    function soarFrameworkConfigurationWizard100Ctrl($scope, widgetUtilityService, $rootScope, widgetBasePath, WizardHandler) {
+      $scope.moveNext = moveNext;
       $scope.isLightTheme = $rootScope.theme.id === 'light';
-      $scope.startPageImage = $scope.isLightTheme ? widgetBasePath +'images/sfsp-start-light.png': widgetBasePath +'images/sfsp-start-dark.png';
+      $scope.startPageImage = $scope.isLightTheme ? widgetBasePath +'images/sfspStartLight.png': widgetBasePath +'images/sfspStartDark.png';
 
       function _handleTranslations() {
         widgetUtilityService.checkTranslationMode($scope.$parent.model.type).then(function () {
@@ -22,6 +23,11 @@
           };
         });
       }
+
+      function moveNext() {
+
+        WizardHandler.wizard('soarFrameworkConfigurationWizard').next();
+    }
 
       function init() {
         // To handle backward compatibility for widget
