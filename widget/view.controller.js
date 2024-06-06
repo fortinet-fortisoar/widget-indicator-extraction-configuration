8fdb8da7fde242a9b20198b5a5104817eb426dfc
cs-rohit-khune <rkhune@fortinet.com>
2024-06-06 19:41:57 +0530

    Added Global Variable Logic

--- widget/view.controller.js
+++ widget/view.controller.js
@@ -16,12 +16,39 @@
     $scope.initList = [];
     $scope.newKeyStoreValue = [];
     $scope.apiQueryPayload = {};
+    $scope.gblVarToKeyStoreMapping = {
+      "Excludelist_IPs": {
+        "keystore": "sfsp-excludelist-ips",
+        "defaultValue": "8.8.8.8,10.1.1.2"
+      },
+      "Excludelist_URLs": {
+        "keystore": "sfsp-excludelist-urls",
+        "defaultValue": "https://www.google.com,https://mail.yahoo.com/login.html,https://www.office.com/"
+      },
+      "Excludelist_Domains": {
+        "keystore": "sfsp-excludelist-domains",
+        "defaultValue": "google.com,yahoo.com,fortinet.net,gmail.com,outlook.com,microsoft.com,fortinet.com,twitter.com,facebook.com,linkedin.com,instagram.com,fortiguard.com,forticloud.com,w3.org"
+      },
+      "Excludelist_Files": {
+        "keystore": "sfsp-excludelist-files",
+        "defaultValue": ""
+      },
+      "Excludelist_Ports": {
+        "keystore": "sfsp-excludelist-ports",
+        "defaultValue": ""
+      },
+      "CIDR_Range": {
+        "keystore": "sfsp-cidr-range",
+        "defaultValue": "10.0.0.0/8,172.16.0.0/12,192.168.0.0/16"
+      }
+    };
     $scope.moveNext = moveNext;
     $scope.moveBack = moveBack;
     $scope.getKeyStoreRecordValues = getKeyStoreRecordValues;
     $scope.isLightTheme = $rootScope.theme.id === 'light';
     $scope.startPageImage = $scope.isLightTheme ? widgetBasePath + 'images/sfsp-start-light.png' : widgetBasePath + 'images/sfsp-start-dark.png';
     $scope.test = test;
+    $scope.buildPayload = buildPayload;
 
     function _handleTranslations() {
       widgetUtilityService.checkTranslationMode($scope.$parent.model.type).then(function () {
@@ -36,7 +63,7 @@
       soarConfigService.getKeyStoreRecord($scope.apiQueryPayload.queryForKeyStore, 'keys').then(function (response) {
         if (response['hydra:member'].length > 0) {
           response['hydra:member'].forEach(function (item) {
-            $scope.keyStoreValue[item.key] = {'recordValue': item.jSONValue, 'recordUUID': item.uuid};
+            $scope.keyStoreValue[item.key] = { 'recordValue': item.jSONValue, 'recordUUID': item.uuid };
           });
           console.log('placeholder');
         }
@@ -64,11 +91,28 @@
       console.log($scope.keyStoreValue);
     }
 
+    function buildPayload(keyStoreName, keyStoreValue) {
+      var apiPayload = soarConfigService.constants().createKeyStorePayload;
+      apiPayload['key'] = keyStoreName;
+      apiPayload['jSONValue'] = keyStoreValue;
+      return apiPayload;
+    }
+
 
     function init() {
-      soarConfigService.getGBLVariable('Excludelist_IPs').then(function(response){
-        $scope.resp = response;
-        console.log($scope.resp);
+      var gblVarList = Object.keys($scope.gblVarToKeyStoreMapping);
+      gblVarList.forEach(function (item) {
+        soarConfigService.getGBLVariable(item).then(function (response) {
+          if (response['hydra:member'].length > 0) {
+            var gblVarName = response['hydra:member'][0].name;
+            var keyStoreName = $scope.gblVarToKeyStoreMapping[gblVarName].keystore;
+            var keyStoreValue = response['hydra:member'][0].value.split(',');
+            var payload = buildPayload(keyStoreName, keyStoreValue);
+            console.log(payload);
+          }
+          $scope.resp = response;
+          console.log($scope.resp);
+        });
       });
       // To handle backward compatibility for widget
       _handleTranslations();
--- widget/widgetAssets/js/soarConfigService.service.js
+++ widget/widgetAssets/js/soarConfigService.service.js
@@ -17,8 +17,10 @@
 
     var service = {
       constants: constants,
-      getKeyStoreRecord: getKeyStoreRecord,
       getGBLVariable: getGBLVariable,
+      removeGBLVariable: removeGBLVariable,
+      createOrUpdateKeyStore: createOrUpdateKeyStore,
+      getKeyStoreRecord: getKeyStoreRecord,
       updateKeyStoreRecord: updateKeyStoreRecord
     }
     return service;
@@ -51,8 +53,49 @@
             "key",
             "jSONValue"
           ]
+        },
+        createKeyStorePayload: {
+          "key": "",
+          "__replace": true,
+          "jSONValue": [],
+          "__fieldsToUpdate": [
+            "jSONValue"
+          ]
         }
       }
+    }
+
+    function getGBLVariable(gblVarName) {
+      var defer = $q.defer();
+      var url = '/api/wf/api/dynamic-variable/?name=' + gblVarName;
+      $resource(url).get(null, function (response) {
+        defer.resolve(response);
+      }, function (err) {
+        defer.reject(err);
+      });
+      return defer.promise;
+    }
+
+    function removeGBLVariable(varName) {
+      var defer = $q.defer();
+      var url = '/api/wf/api/dynamic-variable/?name=' + varName;
+      $resource(url).get(null, function (response) {
+        defer.resolve(response);
+      }, function (err) {
+        defer.reject(err);
+      });
+      return defer.promise;
+    }
+
+    function createOrUpdateKeyStore(queryObject, module) {
+      var defer = $q.defer();
+      var url = API.QUERY + module;
+      $resource(url).save(queryObject, function (response) {
+        defer.resolve(response);
+      }, function (err) {
+        defer.reject(err);
+      })
+      return defer.promise;
     }
 
     function getKeyStoreRecord(queryObject, module) {
@@ -74,18 +117,6 @@
       }).update({ 'jSONValue': keyStoreValue }).$promise.then(function () {
       });
     }
-
-    function getGBLVariable(varName) {
-      var defer = $q.defer();
-      var url = '/api/wf/api/dynamic-variable/?name=' + varName;
-      $resource(url).get(null, function (response) {
-        defer.resolve(response);
-      }, function (err) {
-        defer.reject(err);
-      });
-      return defer.promise;
-    }
-
 
   }
 })();
